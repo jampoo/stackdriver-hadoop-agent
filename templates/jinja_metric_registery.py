49b181e37be38d1c2da6cf47f4ff1f282bb71d0b
Bo Zhang <zhanbo@zhanbo-macbookpro.roam.corp.google.com>
2017-03-07 11:37:12 -0800

    fix the bug to exclude the mbean has no value

--- templates/jinja_metric_registery.py
+++ templates/jinja_metric_registery.py
@@ -5,14 +5,13 @@ import os.path
 import sys
 
 file_path = "./jinja-metric-raw.csv"
-jinja_metric_format = '    ("%s", "%s", "%s"),'
 
 def main() :
     with open(file_path, 'rb') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             if (len(sys.argv) == 1 or row['service'] == sys.argv[1]):
-                jinja_metric_name = jinja_metric_format % (row['service'], row['name'], row['value_type'])
+                jinja_metric_name = '    ("%s", "%s"),' % (row['name'], row['value_type'])
                 print jinja_metric_name
 
 if __name__ == "__main__":
--- templates/render_hadoop_template.py
+++ templates/render_hadoop_template.py
@@ -8,24 +8,25 @@ hadoop_template = 'hadoop-custom-metrics.conf.jinja'
 
 
 def read_metric_list_from_textfile(text_file):
-    services = {}
-    hadoop_metrics_upload_list = []
+    service_dict = {}
+    attribute_list = []
     for line in text_file.readlines():
         # TODO(zhanbo): verify the format
         service_attribute = line.split('.')
-        service = substrs[0]
-        services.add(service)
-        hadoop_metrics_upload_list.append(service_attribute)
-    return (services, hadoop_metrics_upload_list)
+        service = service_attribute[0].strip()
+        if not(service_dict.has_key(service)) :
+            service_dict[service] = '1'  # put dumb value
+        attribute_list.append(line.strip())
+    return (service_dict.keys(), attribute_list)
 
-def create_stackdriver_plugin(metric_list, templates_dir):
+def create_stackdriver_plugin(service_list, attribute_list, templates_dir):
     env = jinja2.Environment(
         loader=jinja2.FileSystemLoader(templates_dir),
         trim_blocks=True,
         lstrip_blocks=True,
         extensions=['jinja2.ext.do'])
 
-    print env.get_template(hadoop_template).render(upload_list = metric_list)
+    print env.get_template(hadoop_template).render(service_list = service_list, attribute_list = attribute_list)
     
 
 def main():
@@ -37,7 +38,8 @@ def main():
     args = parser.parse_args()
     if args.whitelist is not None:
         result = read_metric_list_from_textfile(args.whitelist)
-        create_stackdriver_plugin(metric_list, parse_resultargs.templates_dir)
+        print result
+        create_stackdriver_plugin(result[0], result[1], args.templates_dir)
 
 if __name__ == "__main__":
     main()
